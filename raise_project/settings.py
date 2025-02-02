970e674a43562bc77ba5c6a6e7bc4677a55478f4
nehavijay21 <nehavijay8441@gmail.com>
2025-02-02 19:08:31 +0530

    login new

--- raise_project/settings.py
+++ raise_project/settings.py
@@ -126,4 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # LOGIN_REDIRECT_URL = 'dashboard'  # or the URL name of your choice
 
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = 'login'
+LOGOUT_REDIRECT_URL = '/login/'
